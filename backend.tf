a911616b7ad1d92ce090353e4d13b2324b2bf1fd
Clemens Wältken <clemens.waltken@microsoft.com>
2024-09-25 14:50:02 +0200

    Recreate the backend configuration

--- backend.tf
+++ backend.tf
@@ -1,4 +1,9 @@
 terraform {
   backend "azurerm" {
+    subscription_id      = "685dc6df-f715-40c9-91f3-ecf0aea044bd"
+    resource_group_name  = "dev-terraform-eon-demo-rg"
+    storage_account_name = "ranstoeauntclwaltke"
+    container_name       = "tfstate"
+    key                  = "terraform-eon-demo-dev.tfstate"
   }
 }
